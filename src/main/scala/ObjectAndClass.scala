96f4c42035b44875ca838230b1bc4e544dad9630
Zijian Peng <pengzijian@gmail.com>
2020-04-10 08:17:25 +0800

    test1

--- src/main/scala/ObjectAndClass.scala
+++ src/main/scala/ObjectAndClass.scala
@@ -4,12 +4,23 @@ package classandobject
 class Person(val firstName: String, val lastName: String) {}
 
 object Person {
-  def apply(name: String): Person = ???
+  def apply(name: String): Person = {
+    val names = name.split(" ")
+    return new Person(names(0), names(1))
+  }
 }
 
 // * Pattern Matching
 case class Cat(color: String, food: String)
 
+object Cat {
+  def apply(color: String, food: String): Cat = {
+    return new Cat(color, food)
+  }
+}
+
 object ChipShop {
-  def willServe(c: Cat): Boolean = ???
+  def willServe(c: Cat): Boolean = {
+    return c.food == "Chips"
+  }
 }
