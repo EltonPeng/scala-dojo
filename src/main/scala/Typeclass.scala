01e95cc14346fef2ec65c4153ae114c0a5d054e5
Zijian Peng <pengzijian@gmail.com>
2020-04-11 12:05:12 +0800

    test2.4

--- src/main/scala/Typeclass.scala
+++ src/main/scala/Typeclass.scala
@@ -53,7 +53,9 @@ trait JsonWriter[A] {
 }
 
 object JsonWriter {
-  def write[A](a: A): String = ???
+  def write[A](a: A)(implicit jsonWriter: JsonWriter[A]): String = {
+    jsonWriter.write(a)
+  }
   implicit class Ops[A](a: A) {
     def writeJson: String = ???
   }
@@ -62,21 +64,45 @@ object JsonWriter {
 final case class Person(name: String, email: String)
 
 object Person {
-  implicit val jsonWriterForPerson: JsonWriter[Person] = ???
+  implicit val jsonWriterForPerson: JsonWriter[Person] =
+    new JsonWriter[Person] {
+      override def write(in: Person): String = {
+        val name = in.name
+        val email = in.email
+        s"""{"name": "$name", "email": "$email"}"""
+      }
+    }
   // hint: https://www.scala-lang.org/api/current/scala/math/Ordering$.html#fromLessThan[T](cmp:(T,T)=%3EBoolean):scala.math.Ordering[T]
-  implicit val sortablePerson: Ordering[Person] = ???
+  implicit val sortablePerson: Ordering[Person] = new Ordering[Person] {
+    override def compare(x: Person, y: Person): Int = x.name compare y.name
+  }
 }
 
 final case class Cat(name: String, food: String)
 
 object Cat {
-  implicit val jsonWriterForCat: JsonWriter[Cat] = ???
+  implicit val jsonWriterForCat: JsonWriter[Cat] = new JsonWriter[Cat] {
+    override def write(in: Cat): String = {
+      val name = in.name
+      val food = in.food
+      s"""{"name": "$name", "food": "$food"}"""
+    }
+  }
 }
 
 final case class CatPerson(person: Person, cat: Cat)
 
 object CatPerson {
-  implicit val jsonWriterForCatPerson: JsonWriter[CatPerson] = ???
+  implicit val jsonWriterForCatPerson: JsonWriter[CatPerson] =
+    new JsonWriter[CatPerson] {
+      override def write(in: CatPerson): String = {
+        val pname = in.person.name
+        val pemail = in.person.email
+        val cname = in.cat.name
+        val cfood = in.cat.food
+        s"""{"person":{"name": "$pname", "email": "$pemail"},"cat":{"name": "$cname", "food": "$cfood"}}"""
+      }
+    }
   implicit class CatPersonOps(cp: CatPerson) {
     def writeJson: String = ???
   }
